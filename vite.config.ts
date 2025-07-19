827c3a4aa12ccff638e6a11a7fd3088640670ec8
dwu006 <happy4danielwu@gmail.com>
2025-07-19 15:43:26 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.glb"],
   plugins: [react()],
   server: {
     host: true, // Listen on all addresses including 127.0.0.1 and localhost
