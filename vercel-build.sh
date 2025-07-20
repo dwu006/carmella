9b190148654a0c68766180817c1edc94d91cec28
dwu006 <happy4danielwu@gmail.com>
2025-07-20 01:16:15 -0700

    fixes

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git lfs install
-git remote set-url origin https://github.com/dwu006/carmella.git
+git remote add origin https://github.com/dwu006/carmella.git
 git lfs pull
 npm run build
